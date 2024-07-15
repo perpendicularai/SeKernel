e70eab96ff3d38782f5c6eb2ece2bc58e1ec572f
Where data meets intelligence <146530480+perpendicularai@users.noreply.github.com>
2024-07-15 14:56:37 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    author="Perpendicular AI",
+    author_email = "support@perpendicular.web.za",
+    name="sk_kernel",
+    version="0.0.1",
+    long_description=open('README.md').read(),
+    install_requires=[
+        'streamlit',
+    ],
+)
